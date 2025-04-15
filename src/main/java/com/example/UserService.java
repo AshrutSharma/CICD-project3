d14bcffeeac5a01510ec19203746a1201bd03d69
Ashrut Sharma <ashrut.sharma3333@gmail.com>
2025-04-15 20:16:10 +0530

    Add sayHello method to UserService class

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -5,4 +5,8 @@ public class UserService {
     public String greetUser(String name) {
         return "Hello, " + name + "!";
     }
+    public String sayHello() {
+        return "Hello from feature/add-hello";
+    }
+    
 }
